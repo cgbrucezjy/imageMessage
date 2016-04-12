a70cc7db2702b9a9d864f45fdd41ab2817fc09a0
cgbrucezjy <cgbrucezjy2@gmail.com>
2016-04-12 04:10:04 -0400

    initial commit

---
+++ app/pages/home/home.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor() {
+  		this.imageSrc="http://i164.photobucket.com/albums/u8/hemi1hemi/COLOR/COL9-6.jpg";
+  }
+}
